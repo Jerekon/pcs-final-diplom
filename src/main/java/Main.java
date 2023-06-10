986b9f29d441976805cb074150b4a3d5df82e74d
Jerekon <alexander.rybtsov@gmail.com>
2023-06-10 13:39:17 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ public class Main {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
         System.out.println(engine.search("бизнес"));
 
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+
     }
 }
